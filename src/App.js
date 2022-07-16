57aec503964b62c5403c04ed60da596bdf0f0a81
Igor Potemkin <ipotemkin@rambler.ru>
2022-07-16 13:57:17 +0600

    used useMemo, added ThemeContextProvider, refactored toggleTheme

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-/* eslint-disable react/jsx-no-constructed-context-values */
-
-import { useState } from 'react'
-
 import Console from './components/Console'
 import Title from './components/Title'
 
-import { themes, ThemeContext } from './context/theme'
+import ThemeContextProvider from './context/theme'
 import ThemeSwitcher from './components/theme-switcher'
 
 import { AppWrapper, GlobalStyle } from './styles'
 
 
-const App = () => {
-  const [currentTheme, setCurrentTheme] = useState(themes.green)
-  
-  const toggleTheme = () => {
-    if (currentTheme === themes.green) setCurrentTheme(themes.orange)
-    else setCurrentTheme(themes.green)
-  }
-  
-  return (
-    <ThemeContext.Provider value={{ theme: currentTheme, toggleTheme }}>
-      <AppWrapper>
-        <GlobalStyle />
-        <ThemeSwitcher />
-        <Title>Console CMD. SKYPRO_REACT</Title>
-        <Console />
-      </AppWrapper>
-    </ThemeContext.Provider>
+const App = () => (
+  <ThemeContextProvider>
+    <AppWrapper>
+      <GlobalStyle />
+      <ThemeSwitcher />
+      <Title>Console CMD. SKYPRO_REACT</Title>
+      <Console />
+    </AppWrapper>
+  </ThemeContextProvider>
   )
-}
 
 export default App
--- src/context/theme.jsx
+++ src/context/theme.jsx
@@ -1,20 +1,34 @@
-/* eslint-disable import/prefer-default-export */
-
-import React, { useContext } from "react"; 
+import { useContext, useMemo, useState, createContext } from "react"
 
 export const themes = {
-    green: { color: 'green' },
-    orange: { color: 'orange' }
+  green: { color: 'green' },
+  orange: { color: 'orange' }
 }
 
-export const ThemeContext = React.createContext({
-    theme: themes.green,
-    toggleTheme: () => {},
+export const ThemeContext = createContext({
+  theme: themes.green,
+  toggleTheme: () => {},
 })
 
-export const useThemeContext = () => {
-    const theme = useContext(ThemeContext)
+export const useThemeContext = () => useContext(ThemeContext)
 
-    if (!theme) return themes.green
-    return theme
+const ThemeContextProvider = ({ children }) => {
+  const [currentTheme, setCurrentTheme] = useState(themes.green)
+
+  const toggleTheme = () => setCurrentTheme(
+    prevTheme => prevTheme === themes.green ? themes.orange : themes.green
+  )
+
+  const value = useMemo(
+    () => ({ theme: currentTheme, toggleTheme }),
+    [currentTheme]
+  )
+  
+  return (
+    <ThemeContext.Provider value={value}>
+      {children}
+    </ThemeContext.Provider>
+  )
 }
+
+export default ThemeContextProvider
